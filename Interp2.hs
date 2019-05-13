65f7604aad93b1c03203614fecbabe639ae60fff
adleypc <jadley08@gmail.com>
2019-05-13 16:08:21 -0400

    added church numerals

--- Interp2.hs
+++ Interp2.hs
@@ -55,6 +55,34 @@ valofp ρ []                    = []
 valofp ρ ((ExecD (Def x e)):d) = (valofp (extend ρ x (fromJust (valof ρ e))) d)
 valofp ρ ((ExecE e):d)         = (fromJust (valof ρ e)) : (valofp ρ d)
 
+-- Church ----------------------------
+withchurch :: Expr -> Program
+withchurch e = [ExecD (Def (Id "church-zero")
+                        (Λ (Id "f")
+												  (Λ (Id "x")
+													  (EId (Id "x"))))),
+								ExecD (Def (Id "church-add1")
+								        (Λ (Id "n-1")
+												  (Λ (Id "f")
+													  (Λ (Id "x")
+														  (App (EId (Id "f"))
+															  (App (App (EId (Id "n-1")) (EId (Id "f")))
+																  (EId (Id "x")))))))),
+								ExecD (Def (Id "church-plus")
+								        (Λ (Id "j")
+												  (Λ (Id "k")
+													  (Λ (Id "f")
+														  (Λ (Id "x")
+															  (App (App (EId (Id "j")) (EId (Id "f")))
+																  (App (App (EId (Id "k")) (EId (Id "f")))
+																	  (EId (Id "x"))))))))),
+								ExecE e]
+
+tochurch :: Int -> Expr
+tochurch 0 = EId (Id "church-zero")
+tochurch n = App (EId (Id "church-add1"))
+                 (tochurch (sub1 n))
+
 -- Tests -----------------------------
 main =
 	print (
@@ -71,16 +99,23 @@ main =
 										 (App (EId (Id "y")) (EId (Id "x"))))))
 		&&
 		(valofp
-		  emptyEnv
+			emptyEnv
 			[ExecD (Def (Id "id")
-			         (Λ (Id "x")
-							   (EId (Id "x")))),
+							 (Λ (Id "x")
+								 (EId (Id "x")))),
 			 ExecE (App (EId (Id "id"))
-			         (Λ (Id "y")
-							   (Λ (Id "z")
-								   (App (EId (Id "z"))
-									   (EId (Id "y"))))))]) ==
+							 (Λ (Id "y")
+								 (Λ (Id "z")
+									 (App (EId (Id "z"))
+										 (EId (Id "y"))))))]) ==
 		[Clos [(Id "id",Clos [] (Id "x") (EId (Id "x")))]
-		      (Id "y")
+					(Id "y")
 					(Λ (Id "z") (App (EId (Id "z")) (EId (Id "y"))))]
+		&&
+		(valofp
+			emptyEnv
+			(withchurch (tochurch 0))) ==
+		[Clos []
+		      (Id "f")
+					(Λ (Id "x") (EId (Id "x")))]
 	)
